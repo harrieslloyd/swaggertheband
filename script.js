02b3eaeaa7b526f360cc5a09deaefb6d250cba33
Harries Lloyd <harrieslloyd9@gmail.com>
2023-09-14 18:39:31 -0600

    Added Web Scraping for SwaggerBand Events

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ function screenWidth(){
 function screenHeight(){
     return screen.height;
 }
+
+async function getData(url) {
+    const response = await fetch(url)
+    return response.text
+}
+
+getData("https://docs.google.com/spreadsheets/d/e/2PACX-1vR0RmKzzPAtkwTDAXfQDnhaqqxmLDgcGFZGlX2-0dpkd95u9fVj_jySVVC2j8GVsSQiH-p2w7zvd4vi/pub?gid=0&single=true&output=csv").then((data) => {
+    const events = data
+        .split(/\r?\n/).map((l) => l.split(","))
+        .map((event) => {
+            return { date: event[0], title: event[1]}
+        })
+    console.log(events)
+})
