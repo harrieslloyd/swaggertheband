f5aa126de6850e0ae9cc0915c00289f88f28c6af
Adam Rose <arose3000@icloud.com>
2023-09-18 13:12:46 -0600

    adding rows test

--- script.js
+++ script.js
@@ -5,6 +5,31 @@ function screenHeight(){
     return screen.height;
 }
 
+function addRow(tableID) {  
+    var table = upcomingEvents.getElementById(tableID);  
+    var rowCount = table.rows.length;  
+    var row = table.insertRow(rowCount);  
+    //Column 1  
+    var cell1 = row.insertCell(0);  
+    var element1 = document.createElement("input");  
+    element1.type = "button";  
+    var btnName = "button" + (rowCount + 1);  
+    element1.name = btnName;  
+    element1.setAttribute('value', 'Delete'); // or element1.value = "button";  
+    element1.onclick = function() {  
+        removeRow(btnName);  
+    }  
+    cell1.appendChild(element1);  
+    //Column 2  
+    var cell2 = row.insertCell(1);  
+    cell2.innerHTML = rowCount + 1;  
+    //Column 3  
+    var cell3 = row.insertCell(2);  
+    var element3 = document.createElement("input");  
+    element3.type = "text";  
+    cell3.appendChild(element3);  
+}  
+
 async function getData(url) {
     const response = await fetch(url)
     return response.text
