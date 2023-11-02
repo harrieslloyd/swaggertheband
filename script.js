e6e5f9db0e9ae93a7194fd2988bb69c7a45d5888
Harries Lloyd <harrieslloyd9@gmail.com>
2023-11-02 13:49:18 -0600

    now gets access to events!

--- script.js
+++ script.js
@@ -1,87 +1,3 @@
-const clientId = "2f380f8fc28b4ab68298d967fe13805d";
-const params = new URLSearchParams(window.location.search);
-const code = params.get("code");
-
-if (!code) {
-    redirectToAuthCodeFlow(clientId);
-} else {
-    const accessToken = await getAccessToken(clientId, code);
-    const topTracks = await fetchTopTracks(accessToken);
-    populateUI(topTracks);
-}
-
-async function redirectToAuthCodeFlow(clientId) {
-    const verifier = generateCodeVerifier(128);
-    const challenge = await generateCodeChallenge(verifier);
-
-    localStorage.setItem("verifier", verifier);
-
-    const params = new URLSearchParams();
-    params.append("client_id", clientId);
-    params.append("response_type", "code");
-    params.append("redirect_uri", "http://localhost:5173/callback");
-    params.append("scope", "user-read-private user-read-email");
-    params.append("code_challenge_method", "S256");
-    params.append("code_challenge", challenge);
-
-    document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
-}
-
-function generateCodeVerifier(length) {
-    let text = '';
-    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-    for (let i = 0; i < length; i++) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    return text;
-}
-
-async function generateCodeChallenge(codeVerifier) {
-    const data = new TextEncoder().encode(codeVerifier);
-    const digest = await window.crypto.subtle.digest('SHA-256', data);
-    return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
-        .replace(/\+/g, '-')
-        .replace(/\//g, '_')
-        .replace(/=+$/, '');
-}
-
-export async function getAccessToken(clientId, code) {
-    const verifier = localStorage.getItem("verifier");
-
-    const params = new URLSearchParams();
-    params.append("client_id", clientId);
-    params.append("grant_type", "authorization_code");
-    params.append("code", code);
-    params.append("redirect_uri", "http://localhost:5173/callback");
-    params.append("code_verifier", verifier);
-
-    const result = await fetch("https://accounts.spotify.com/api/token", {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: params
-    });
-
-    const { access_token } = await result.json();
-    return access_token;
-}
-
-async function fetchTopTracks(token) {
-    const result = await fetch("https://api.spotify.com/v1/artists/7tPoZvl7OYT2rQDdzCQpfR/top-tracks?market='ES'", {
-        method: "GET", headers: { Authorization: `Bearer ${token}` }
-    });
-
-    console.log(result)
-
-    return await result.json();
-}
-
-function populateUI(topTracks) {
-    console.log(topTracks.name)
-    document.getElementById("topSong").innerText = topTracks.name;
-}
-
-
 function screenWidth(){
     return screen.width;
 }
@@ -102,7 +18,22 @@ function addRow(tableID) {
     const newRow = elem.after(clone);
 } 
 
-async function downloadCSV(){
+function altAddRow(tableID, date, desc) {
+    console.log("running addRow");
+    const table = document.getElementById(tableID);
+    var row = table.insertRow(-1);
+    row.innerHTML = 
+    `
+    <td><div class="event" id="event0">
+                    <table><tr>
+                        <td>${date}</td>
+                        <td>${desc}</td>
+                    </tr></table>
+                </div></td>
+    `
+}
+
+async function download(){
     try {
         const target = `https://docs.google.com/spreadsheets/d/e/2PACX-1vR0RmKzzPAtkwTDAXfQDnhaqqxmLDgcGFZGlX2-0dpkd95u9fVj_jySVVC2j8GVsSQiH-p2w7zvd4vi/pub?gid=0&single=true&output=csv`; //file
         //const target = `https://SOME_DOMAIN.com/api/data/log_csv?$"queryString"`; //target can also be api with req.query
@@ -116,9 +47,8 @@ async function downloadCSV(){
         });
 
         if (res.status === 200) {
-
             const data = await res.text();
-            console.log(data);
+            return data;
 
         } else {
             console.log(`Error code ${res.status}`);
@@ -127,6 +57,24 @@ async function downloadCSV(){
         console.log(err)
     }
 }
+
+
+  
+
+let a = await download();
+
+//dont ask me how this works
+const lineRegex = /((\\\n)|[^\n])+/g;
+const datumRegex = /,?(("(\\"|.)+?")|([^",][^,]*))?/g;
+const x = a.match(lineRegex).map((row) => 
+    row.match(datumRegex).map((datum) => datum.replace(/^,?"?|"$/g, "").trim()),
+);
+
+x.forEach(rowAdd)
+function rowAdd(item){
+    altAddRow("eventtable", item[0], item[1])
+}
+
 
 
 // getData("").then((data) => {
@@ -138,7 +86,7 @@ async function downloadCSV(){
 //     console.log(data)
 // })
 
-hiddenlogo = document.getElementById("hiddenlogo")
+var hiddenlogo = document.getElementById("hiddenlogo")
 var myScrollFunc = function() {
     var y = window.scrollY;
     if (y >= 300) {
